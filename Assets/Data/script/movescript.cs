5c5bf69474552b856f8e768f9b5b2bb6ce1ff87e
Kanika <Kanika.modi@gmail.com>
2018-02-07 23:40:35 +0530

    Initial commit

---
+++ Assets/Data/script/movescript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movescript : MonoBehaviour {
+    public float moveX, moveY, moveZ;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(moveX, moveY, moveZ) * Time.deltaTime);
+	}
+}
